e85891ef2dc4bf80f5ce667de57b8d960a2fcc14
phillip1029 <ppeng08@gmail.com>
2021-03-01 21:50:12 -0500

    Create dock_run.sh

---
+++ jupyterlab/dock_run.sh
@@ -0,0 +1,5 @@
+# run in windows commandline (CMD) 
+# docker run -p 8888:8888 -v %cd%:/opt/app/data ppeng1029/jupyterlab:py3.8
+
+# run in powershell
+# docker run -p 8888:8888 -v ${PWD}:/opt/app/data ppeng1029/jupyterlab:py3.8
